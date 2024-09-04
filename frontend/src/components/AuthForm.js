92feabbdc86755c63b10fa7214419039613b2ada
Yeganeh <56545979+yegiii@users.noreply.github.com>
2024-09-04 16:14:12 +0330

    Add showing error in auth form

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,8 +1,11 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import { Form, Link, useSearchParams, useActionData } from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
+  const data = useActionData();
+  // if in action we return a response, we will have value for useActionData()
+
   const [searchParams, setSearchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
 
@@ -10,6 +13,14 @@ function AuthForm() {
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
