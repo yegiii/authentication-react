8a9fb466c3d93cf7cbae64a32f22d7fa5d04404b
Yeganeh <56545979+yegiii@users.noreply.github.com>
2024-09-04 17:00:14 +0330

    Adding user logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 import Athentication, { action as authAction } from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
 
 const router = createBrowserRouter([
   {
@@ -60,6 +61,10 @@ const router = createBrowserRouter([
         path: "newsletter",
         element: <NewsletterPage />,
         action: newsletterAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
       },
     ],
   },
